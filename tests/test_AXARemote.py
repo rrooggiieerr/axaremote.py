7519e79d7729aada18ca3d2284e7562b0935acce
Rogier van Staveren <rogier@batoid.com>
2022-11-25 21:15:03 +0100

    Refactoring and more unit tests

--- tests/test_AXARemote.py
+++ tests/test_AXARemote.py
@@ -7,7 +7,7 @@ import logging
 import time
 import unittest
 
-from axaremote.axaremote import AXARemote
+from axaremote import AXARemote
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -18,46 +18,168 @@ serial_port = "/dev/tty.usbserial-110"
 
 
 class Test(unittest.TestCase):
+    _axa = None
+
+    def setUp(self):
+        self._axa = AXARemote(serial_port)
+        self._axa.connect()
+        status = self._axa.status()
+        if status != AXARemote.STATUS_LOCKED:
+            logger.info("Resetting AXA Remote to Locked Position")
+            self._axa.close()
+            time.sleep(AXARemote._TIME_CLOSE + AXARemote._TIME_LOCK)
+            self._axa.sync_status()
+            self._axa.stop()
+
+    def tearDown(self):
+        if self._axa is not None:
+            self._axa.sync_status()
+            status = self._axa.status()
+            if status != AXARemote.STATUS_LOCKED:
+                logger.info("Resetting AXA Remote to Locked Position")
+                self._axa.close()
+                time.sleep(AXARemote._TIME_CLOSE + AXARemote._TIME_LOCK)
+                self._axa.disconnect()
+                self._axa = None
+
     def testConnect(self):
-        axa = AXARemote(serial_port)
-        response = axa.connect()
+        response = self._axa.connect()
         self.assertTrue(response)
-        self.assertIsNotNone(axa._connection)
-        self.assertIsNotNone(axa.device)
-        self.assertIsNotNone(axa.version)
+        self.assertIsNotNone(self._axa._connection)
+        self.assertIsNotNone(self._axa.device)
+        self.assertIsNotNone(self._axa.version)
 
     def testDisconnect(self):
-        axa = AXARemote(serial_port)
-        axa.connect()
-        response = axa.disconnect()
+        response = self._axa.disconnect()
         self.assertTrue(response)
-        self.assertIsNone(axa._connection)
+        self.assertIsNone(self._axa._connection)
 
     def testOpen(self):
-        axa = AXARemote(serial_port)
-        axa.connect()
-        response = axa.open()
+        response = self._axa.open()
         self.assertTrue(response)
-        axa.close()
+        self._axa.stop()
+
+    def testUnlocking(self):
+        self._axa.open()
+        time.sleep(AXARemote._TIME_UNLOCK / 2)
+        status = self._axa.status()
+        self.assertIs(AXARemote.STATUS_UNLOCKING, status)
+        self._axa.stop()
 
     def testStop(self):
-        axa = AXARemote(serial_port)
-        axa.connect()
-        axa.open()
-        time.sleep(5)
-        response = axa.stop()
+        self._axa.open()
+        time.sleep(AXARemote._TIME_UNLOCK + (AXARemote._TIME_OPEN / 2))
+        response = self._axa.stop()
         self.assertTrue(response)
-        axa.close()
 
     def testClose(self):
-        axa = AXARemote(serial_port)
-        axa.connect()
-        axa.open()
-        time.sleep(5)
-        response = axa.close()
+        self._axa.open()
+        time.sleep(AXARemote._TIME_UNLOCK + AXARemote._TIME_OPEN + 1)
+        response = self._axa.close()
         self.assertTrue(response)
+
+    def testClosing(self):
+        self._axa.open()
+        time.sleep(AXARemote._TIME_UNLOCK + AXARemote._TIME_OPEN + 1)
+        self._axa.close()
+        time.sleep(AXARemote._TIME_CLOSE / 2)
+        status = self._axa.status()
+        self.assertIs(AXARemote.STATUS_CLOSING, status)
+        position = self._axa.position()
+        self.assertAlmostEqual(50.0, position, delta=1)
+
+    def testLocking(self):
+        self._axa.open()
+        time.sleep(AXARemote._TIME_UNLOCK + AXARemote._TIME_OPEN + 1)
+        self._axa.close()
+        time.sleep(AXARemote._TIME_CLOSE + (AXARemote._TIME_LOCK / 2))
+        status = self._axa.status()
+        self.assertIs(AXARemote.STATUS_LOCKING, status)
+
+    def testUpdateUnlocking(self):
+        self._axa._status = AXARemote.STATUS_UNLOCKING
+        self._axa._position = 0
+        self._axa._timestamp = time.time()
+        time.sleep(self._axa._TIME_UNLOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_UNLOCKING, self._axa._status)
+        self.assertAlmostEqual(25.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_UNLOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_UNLOCKING, self._axa._status)
+        self.assertAlmostEqual(50.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_UNLOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_UNLOCKING, self._axa._status)
+        self.assertAlmostEqual(75.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_UNLOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_OPENING, self._axa._status)
+        self.assertAlmostEqual(0.0, self._axa._position, delta=1)
+
+    def testUpdateOpening(self):
+        self._axa._status = AXARemote.STATUS_OPENING
+        self._axa._position = 0
+        self._axa._timestamp = time.time() - self._axa._TIME_UNLOCK
+        time.sleep(self._axa._TIME_OPEN / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_OPENING, self._axa._status)
+        self.assertAlmostEqual(25.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_OPEN / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_OPENING, self._axa._status)
+        self.assertAlmostEqual(50.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_OPEN / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_OPENING, self._axa._status)
+        self.assertAlmostEqual(75.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_OPEN / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_OPEN, self._axa._status)
+        self.assertEqual(100.0, self._axa._position)
+
+    def testUpdateClosing(self):
+        self._axa._status = AXARemote.STATUS_CLOSING
+        self._axa._position = 100
+        self._axa._timestamp = time.time()
+        time.sleep(self._axa._TIME_CLOSE / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_CLOSING, self._axa._status)
+        self.assertAlmostEqual(75.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_CLOSE / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_CLOSING, self._axa._status)
+        self.assertAlmostEqual(50.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_CLOSE / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_CLOSING, self._axa._status)
+        self.assertAlmostEqual(25.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_CLOSE / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_LOCKING, self._axa._status)
+        self.assertAlmostEqual(100.0, self._axa._position, delta=1)
+
+    def testUpdateLocking(self):
+        self._axa._status = AXARemote.STATUS_LOCKING
+        self._axa._position = 100
+        self._axa._timestamp = time.time() - self._axa._TIME_CLOSE
+        time.sleep(self._axa._TIME_LOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_LOCKING, self._axa._status)
+        self.assertAlmostEqual(75.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_LOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_LOCKING, self._axa._status)
+        self.assertAlmostEqual(50.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_LOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_LOCKING, self._axa._status)
+        self.assertAlmostEqual(25.0, self._axa._position, delta=1)
+        time.sleep(self._axa._TIME_LOCK / 4)
+        self._axa._update()
+        self.assertIs(AXARemote.STATUS_LOCKED, self._axa._status)
+        self.assertEqual(0.0, self._axa._position)
 
 
 if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
